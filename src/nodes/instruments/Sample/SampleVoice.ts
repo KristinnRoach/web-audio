6db94e0390d3902554975a8a3fb1c3203e67fede
kiddi <kristinnroach@gmail.com>
2026-09-08 19:33:41 +0000

    add offset to glideFromMidiNote in trigger()

--- src/nodes/instruments/Sample/SampleVoice.ts
+++ src/nodes/instruments/Sample/SampleVoice.ts
@@ -364,7 +364,9 @@ export class SampleVoice {
     this.#am_lfo?.setMusicalNote(midiNote + this.#am_lfo_semitone_offset, {
       divisor: 1,
       glideTime: scaledGlideTime,
-      glideFromMidiNote: options?.glide?.prevMidiNote,
+      glideFromMidiNote: options?.glide?.prevMidiNote
+        ? options.glide.prevMidiNote + this.#am_lfo_semitone_offset
+        : undefined,
       timestamp,
     });
 
